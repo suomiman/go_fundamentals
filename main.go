b140f5a6836b1ef70c187a059133ba5f5077e30e
Jyrki Von Karkki <36864172+suomiman@users.noreply.github.com>
2026-09-25 22:40:30 +0100

    lesson 02

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func main() {
-    var first, second string
-    fmt.Scan(&first)
-    fmt.Scan(&second)
-    // Print one greeting per line, first name first.
-    fmt.Println("Hello,", first)
-    fmt.Println("Hello,", second)
+	var first, second int
+	fmt.Scan(&first)
+	fmt.Scan(&second)
+  fmt.Println(first + second)
+  fmt.Println(first - second)
+  fmt.Println(first == second)
+
+	// TODO: line 1 - print the sum of the two integers.
+	// TODO: line 2 - print the result of subtracting the second from the first.
+	// TODO: line 3 - print whether the two integers are equal.
 }
 
